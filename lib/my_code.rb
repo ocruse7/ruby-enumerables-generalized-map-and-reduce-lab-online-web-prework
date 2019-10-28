5676d5f325e9a863cc41cfe1335a8c4a26bc250b
Owen Cruse <ocruse7@gmail.com>
2019-10-28 04:30:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# Your Code Here
+def map(x)
+  new = []
+  i = 0
+  
+  while i < x.length
+    new.push( yield( x[i] ) )
+    i += 1
+  end
+  
+  new
+end
+
+def reduce( x, sp=nil )
+  
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = x[0]
+    i = 1
+  end
+  
+  while i < x.length
+    accum = yield( accum, x[i] )
+    i += 1
+  end
+  
+  accum
+end
